14d511be5efcc04fe8517988482d25d7645aa838
Atmakuri Tagore Nandan <tagorenandan17@gmail.com>
2026-06-16 23:39:55 +0530

    implemented realistic simulation - got the battery metric drained with influence

--- backend/app/simulator.py
+++ backend/app/simulator.py
@@ -1,4 +1,5 @@
 from app.schemas import FlightPhase
+import random
 
 
 class DroneSimulator:
@@ -31,17 +32,15 @@ class DroneSimulator:
             FlightPhase.POSTFLIGHT: 10,
         }
 
+
     def tick(self):
         self.phase_elapsed += 1
 
-        print(
-            f"Phase={self.phase}, elapsed={self.phase_elapsed}, "
-            f"limit={self.phase_durations[self.phase]}"
-        )
-
         if self.phase_elapsed >= self.phase_durations[self.phase]:
-            print("ADVANCING PHASE")
             self.advance_phase()
+
+        self.update_telemetry()
+
 
 
     def advance_phase(self):
@@ -61,4 +60,47 @@ class DroneSimulator:
         if current_index < len(phases) - 1:
             self.phase = phases[current_index + 1]
             self.phase_elapsed = 0
+
+    def update_telemetry(self):
+        if self.phase == FlightPhase.PREFLIGHT:
+            self.altitude = 0
+            self.airspeed = 0
+
+        elif self.phase == FlightPhase.TAKEOFF:
+            self.altitude += 6
+            self.airspeed = 8
+
+        elif self.phase == FlightPhase.CLIMB:
+            self.altitude += 10
+            self.airspeed = 15
             
+        elif self.phase == FlightPhase.CRUISE:
+            self.altitude += random.uniform(-1, 1)
+            self.airspeed = random.uniform(18, 22)
+
+        elif self.phase == FlightPhase.LOITER:
+            self.altitude += random.uniform(-2, 2)
+            self.airspeed = random.uniform(8, 12)
+
+        elif self.phase == FlightPhase.RTB:
+            self.airspeed = 18
+
+            self.latitude -= 0.00005
+            self.longitude -= 0.00005
+
+        elif self.phase == FlightPhase.LANDING:
+            self.altitude = max(0, self.altitude - 12)
+            self.airspeed = 5
+
+        elif self.phase == FlightPhase.POSTFLIGHT:
+            self.altitude = 0
+            self.airspeed = 0
+
+        self.battery = max(0, self.battery - 0.15)
+        self.rssi = max(
+            20,
+            100 - (self.altitude * 0.15)
+        )
+
+        self.pitch = random.uniform(-10, 10)
+        self.roll = random.uniform(-15, 15)
--- backend/test_simulator.py
+++ backend/test_simulator.py
@@ -2,10 +2,12 @@ from app.simulator import DroneSimulator
 
 sim = DroneSimulator()
 
-for i in range(50):
+for i in range(60):
     sim.tick()
+
     print(
-        f"Tick={i} "
-        f"Phase={sim.phase.value} "
-        f"Elapsed={sim.phase_elapsed}"
+        sim.phase.value,
+        round(sim.altitude, 1),
+        round(sim.airspeed, 1),
+        round(sim.battery, 1)
     )
